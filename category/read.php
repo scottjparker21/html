8d850b2570f3b73c3b8a1530724c4c400b52fcde
scottjparker21 <scottjparker21@gmail.com>
2015-12-06 20:18:35 -0600

    pushing staging to AWS through Github

---
+++ category/read.php
@@ -0,0 +1,54 @@
+<?php
+require 'database.php';
+    $id = null;
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( null==$id ) {
+        header("Location: index.php");
+    } else {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM category where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        Database::disconnect();
+    }
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Read a Category</h3>
+                    </div>
+                     
+                    <div class="form-horizontal" >
+                      <div class="control-group">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <label class="checkbox">
+                                <?php echo $data['name'];?>
+                            </label>
+                        </div>
+                      </div>
+                        <div class="form-actions">
+                     
+                      
+                    </div>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
