fdfed4ec961823d4c6f613c117aec9a2f2c3ea9b
scottjparker21 <scottjparker21@gmail.com>
2015-12-08 11:53:04 -0600

    pushing staging to AWS through Github

--- navbar.php
+++ navbar.php
@@ -1,4 +1,16 @@
-	
+	<?php
+			$pdo = Database::connect();
+	        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	        $sql = "SELECT id,name FROM category ORDER BY name";
+	        $q = $pdo->prepare($sql);
+	        $q->execute());
+	        $categories = $q->fetchAll();
+	        print_r($categories);
+	        
+	        Database::disconnect(); 
+	 ?>
+
+	 <html>
 	<nav class="navbar navbar-inverse">
   		<div class="container-fluid">
     		<div class="navbar-header">
@@ -13,12 +25,12 @@
       			<ul class="nav navbar-nav">
        			<li><a href="index.php">Home</a></li>
         		<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Products<span class="caret"></span></a>
-        				<!-- <ul class="dropdown-menu">
+        				 <!-- <ul class="dropdown-menu">
         					
         					<?php foreach ($categories as $row){?><li "<?php echo $row['id'];?>"><?php echo $row['name'];?></li><?php }?>
         					
 
-        				</ul> -->
+        				</ul>  -->
         		<li><a href="cart.php">Cart</a></li>
         		<li><a href="search.php">Search</a></li>
             	<li><a href=""></a></li>
@@ -30,5 +42,5 @@
     		</div>
   		</div>
 	</nav>
-
+	</html
 
