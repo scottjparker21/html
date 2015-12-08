2ef2404c7e228735ec1805867272f2ffc90e4ae8
scottjparker21 <scottjparker21@gmail.com>
2015-12-07 19:10:30 -0600

    pushing staging to AWS through Github

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		</head>
+		<body>
+				<?php
+					require 'navbar.php';
+				?>
+
+		</body>
+		</html>
+
---
+++ navbar.php
@@ -0,0 +1,28 @@
+
+	<nav class="navbar navbar-inverse">
+  		<div class="container-fluid">
+    		<div class="navbar-header">
+    			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>
+      			</button>
+      			<a class="navbar-brand" href="#"><h5> Milwaukee Glassware </h5><img src="MKE Glass" alt="" id="navlogo"></a>
+    		</div>
+    		<div class="collapse navbar-collapse" id="myNavbar">
+      			<ul class="nav navbar-nav">
+       			<li><a href="index.php">Home</a></li>
+        		<li><a href="category.php">Products</a></li>
+        		<li><a href="cart.php">Cart</a></li>
+        		<li><a href="search.php">Search</a></li>
+            <li><a href=""></a></li>
+      			</ul>
+    			<ul class="nav navbar-nav navbar-right">
+        		<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        		<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      			</ul>
+    		</div>
+  		</div>
+	</nav>
+
+
