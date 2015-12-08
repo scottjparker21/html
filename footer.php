8204d917d4360447f4521dc6c4f9e5ec3bb205f9
scottjparker21 <scottjparker21@gmail.com>
2015-12-07 20:05:06 -0600

    pushing staging to AWS through Github

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+	  <footer class="footer">
+      		<div class="container">
+        		<p class="text-muted">Place sticky footer content here.</p>
+     		 </div>
+      </footer>
+
+    <script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+	
--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 		<html lang="en">
 		<head>
+				<?php 
+						require 'header.php';
+				?>
 		</head>
 		<body>
 				<?php
-					require 'navbar.php';
+						require 'navbar.php';
 				?>
+				
+				<!-- END NAVBAR -->
 
+
+
+
+				<!-- STICKY FOOTER -->
+
+				<?php
+						require 'footer.php'
+				?>
 		</body>
 		</html>
 
