4a0f36c549ba32a380d61ab7a285c41bf6cffceb
scottjparker21 <scottjparker21@gmail.com>
2015-12-09 17:52:50 -0600

    pushing staging to AWS through Github

--- product.php
+++ product.php
@@ -7,13 +7,15 @@
 				<?php	
 					$id = $_GET['subcatid'];
 			        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			        $sql = "SELECT * FROM product where subcategory_id = ? ";
+			        $sql = "SELECT * FROM product where id = ? ";
 			        $q = $pdo->prepare($sql);
 			        $q->execute(array($id));
-
-			        $product = $q->fetchAll();
+			        $data = $q->fetch(PDO::FETCH_ASSOC);
+			        $name = $data['name'];
+			        $cost = $data['cost'];
+			        $description = $data['description']; 
 			        // print_r($catinfo);
-			    
+			    print_r($data);
 			?>
 					<h3> Products </h3>
 
