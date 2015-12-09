7578c61303294bc642d3db374308422dc45064d6
scottjparker21 <scottjparker21@gmail.com>
2015-12-09 13:59:08 -0600

    pushing staging to AWS through Github

--- category.php
+++ category.php
@@ -9,7 +9,7 @@
 			<?php	
 					$id = $_GET['catid'];
 			        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			        $sql = "SELECT * FROM subcategory where category_id = ? ";
+			        $sql = "SELECT id, name FROM subcategory where category_id = ? ";
 			        $q = $pdo->prepare($sql);
 			        $q->execute(array($id));
 
