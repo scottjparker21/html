b7187819441574337e28028cc2ff06c50ef64c1d
scottjparker21 <scottjparker21@gmail.com>
2015-12-09 17:43:55 -0600

    pushing staging to AWS through Github

--- product.php
+++ product.php
@@ -4,6 +4,25 @@
 		<body>
 			<?php require 'navbar.php';?>
 
+				<?php	
+					$id = $_GET['subcatid'];
+			        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			        $sql = "SELECT * FROM product where subcategory_id = ? ";
+			        $q = $pdo->prepare($sql);
+			        $q->execute(array($id));
+
+			        $product = $q->fetchAll();
+			        // print_r($catinfo);
+			    
+			?>
+					<h3> Products </h3>
+
+			       <?php foreach ($product as $row){?>
+
+			       				{?><li id="<?php echo $row['id'];?>"><a href="product.php?subcatid=<?php echo $row['id'];?>"><?php echo $row['name'];?></a>
+
+			       	<?php } ?>
+
 
 
 
