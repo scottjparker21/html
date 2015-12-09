013f5ac1371897117cdea80bc5205b52664d1e86
scottjparker21 <scottjparker21@gmail.com>
2015-12-09 13:57:59 -0600

    pushing staging to AWS through Github

--- category.php
+++ category.php
@@ -21,7 +21,7 @@
 
 			       <?php foreach ($subcategories as $row){?>
 
-			       				<a href="subcategory.php?catid=<?php echo $row['id']?>"?><?php echo $row['name'];?></a>
+			       				{?><li id="<?php echo $row['id'];?>"><a href="subcategory.php?catid=<?php echo $row['id'];?>"><?php echo $row['name'];?></a>
 
 			       	<?php } ?>
         											
