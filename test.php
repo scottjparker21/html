362c78633572195ed19a8d6f3526eb7f5ec87255
scottjparker21 <scottjparker21@gmail.com>
2015-12-12 13:55:06 -0600

    pushing staging to AWS through Github

--- test.php
+++ test.php
@@ -4,10 +4,10 @@
 
 					echo "hello";
 
-
+					require 'database.php';
 					$pdo = Database::connect();
 					$id = $_GET['entry'];
-/*			        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			        $sql = "SELECT * FROM product WHERE name  LIKE ? ";
 			        $q = $pdo->prepare($sql);
 			        $q->execute(array($id));
