a026c552a5ae94f4d5c96c2287d8c0e9abea4760
scottjparker21 <scottjparker21@gmail.com>
2015-12-12 12:45:44 -0600

    pushing staging to AWS through Github

--- test.php
+++ test.php
@@ -1,15 +1,33 @@
+<!DOCTYPE html>
+		<html lang="en">
+		<head>
+				<?php require 'navbar.php';?>
+		</head>
+		<body>
+				<?php require 'header.php';?>
 
+				<!-- END NAVBAR -->
 
-	 <?php	
-					$id = $_GET['inp'];
-					print_r($id);
-			        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			        $sql = "SELECT * FROM product WHERE name LIKE ? ";
-			        $q = $pdo->prepare($sql);
-			        $q->execute(array($id));
+					 <?php	
+								
+								$id = $_GET['inp'];
+								print_r($id);
+						        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+						        $sql = "SELECT * FROM product WHERE name LIKE ? ";
+						        $q = $pdo->prepare($sql);
+						        $q->execute(array($id));
 
-			        $product = $q->fetchAll();
-			        // print_r($catinfo);
+						        $product = $q->fetchAll();
+			        
 			    
-		
-	?>
+					?>
+
+
+
+
+				<!-- STICKY FOOTER -->
+
+				<?php require 'footer.php';?>
+		</body>
+		</html>
+
