d5886a65ad6d77b97ddb6d654298ea8f92ddb55c
scottjparker21 <scottjparker21@gmail.com>
2015-12-12 10:35:18 -0600

    pushing staging to AWS through Github

--- js/search.js
+++ js/search.js
@@ -2,15 +2,34 @@
 $( document ).ready(function() {
 
 
-	function search (){
-		$.get( "test.php?vari=4", function( data ) {
+	function search (inp){
+		$.get( "test.php?entry=" + inp, function( data ) {
 		  	alert( "Data Loaded: " + data );
 		});
 	}
-		$('#search').keyup(function(e) {
-                     if(e.keyCode !=='') {
-                        search();
-	// call search on input change
-							}
-					});		
+
+	$('#search').keyup(function() {
+		var entry = $(this).val();
+        if(entry !== '' && entry !== null) {
+            search(entry);
+		}
+	});		
+
 });
+
+
+
+/*
+user changes search input
+a function grabs the new value on change
+the function calls a php page with get/ajax
+the php page echos back search results
+the contents of the container on the page are temporarilly stored in a variable
+the contents of container are replaced with what gets echo'd back from the php
+if the php doesn't echo any products, dusplay "no results"
+if the search bar is empty, replace the contents of the page from the temp variable and set the variable to null
+
+
+
+
+*/
