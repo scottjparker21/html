69ebe9ec4855e145a7b75ab385565804b54340ce
scottjparker21 <scottjparker21@gmail.com>
2015-12-14 18:31:01 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 		<?php require 'header.php';?>
 		<body>
 			<?php require 'navbar.php';?>
+			<div id="container">
 
 				<div class="results"></div>	
+
+				<p> This will hopefully been hidden when search field is not empty. </p>
 
 
 
 
 			<?php require 'footer.php';?>
+			</div>
 		</body>
 	</html>
 
--- js/search.js
+++ js/search.js
@@ -2,16 +2,16 @@
 $( document ).ready(function() {
 
 
-	function search (inp){
-		
+	function search (inp){		
 		$.get( "test.php?entry=" + inp, function( data ) {
-		  		$( ".results" ).html(data);
+		  	$( ".results" ).html(data);
 		});
 	}
 
 	$('#search').keyup(function() {
 		var empty = '';
 		var entry = $(this).val();
+
 		if (entry === empty){
 			console.log('empty');
 		}
