ae316a62ed3e03aa45b14f933f555e9cab71c31a
scottjparker21 <scottjparker21@gmail.com>
2015-12-15 19:39:11 -0600

    pushing staging to AWS through Github

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
 	function logmein() {
 		var username = $("#inputUser").val();
 		var password = $("#inputPass").val();
-		$.get('validate.php?username='+username+'&password='+password, function(data) {
+		$.get('gvalidate.php?username='+username+'&password='+password, function(data) {
 			console.log("getting data");
 			console.log(data);
 		});
