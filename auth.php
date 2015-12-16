be1552354f1729167f42ed748a7eb9032365a447
scottjparker21 <scottjparker21@gmail.com>
2015-12-15 19:59:05 -0600

    pushing staging to AWS through Github

--- auth.php
+++ auth.php
@@ -1,25 +1,26 @@
 <?php
-			// 	require 'database.php';
-			// $pdo = Database::connect();
-	$user = $_GET['user'];
-	$pass = $_GET['pass'];
-	echo "Username: " . $user;
-	echo "Password: " . $pass;
-	  //       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	  //       $sql = "SELECT * FROM customer WHERE username = ? ";
-	  //       $q = $pdo->prepare($sql);
-	  //       $q->execute(array($user));
-	  //       $customer = $q->fetchAll();
-	  //       Database::disconnect(); 
+			require 'database.php';
+			$pdo = Database::connect();
+			$user = $_GET['user'];
+			$pass = $_GET['pass'];
+			echo "Username: " . $user;
+			echo "Password: " . $pass;
+	        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	        $sql = "SELECT * FROM customer WHERE username = ? ";
+	        $q = $pdo->prepare($sql);
+	        $q->execute(array($user));
+	        $customer = $q->fetchAll();
+	        Database::disconnect(); 
 
-	  //       $results = '';
+	        $results = '';
 
-	  //      foreach ($customer as $row){ 
+	       foreach ($customer as $row){ 
 			//        	   $results .= "<p>";
-			//        	   $results .= $row['username'];
-			//        	   $results .= $row['password'];
+			       	   $results .= $row['username'];
+			       	   $results .= $row['password'];
 			//        	   $results .= "</p>";
-		 //       		}
+		       		}
 		       		
+		      echo $results;
 		       		
 ?>
--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
+
+
 $(document).ready(function() {
 	$('#send').on("click",function(){
 		var user = $("#inputUser").val();
 		var pass = $("#inputPass").val();
 		$.get('auth.php?user='+user+'&pass='+pass, function(data) {
-			alert(data);
+			console.log(data);
 		});
 	});
 });
