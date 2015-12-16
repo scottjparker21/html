a66256d1e23fa99092b9ca1f7430dabc387b6fdd
scottjparker21 <scottjparker21@gmail.com>
2015-12-15 19:38:38 -0600

    pushing staging to AWS through Github

--- login.php
+++ login.php
@@ -1,34 +1,31 @@
 <!DOCTYPE html>
-		<html lang="en">
-		<head>
-				<?php require 'navbar.php';?>
-		</head>
+	<html lang="en">
+		<?php require 'header.php'; ?>
 		<body>
-				<?php require 'header.php';?>
+			<?php require 'navbar.php'; ?>
+			<form class="form-horizontal">
+		  		<div class="control-group">
+		    		<label class="control-label" for="inputUsername">Username</label>
+		    		<div class="controls">
+		      			<input type="text" id="inputUser" placeholder="Username">
+		    		</div>
+		  		</div>
+		  		<div class="control-group">
+		    		<label class="control-label" for="inputPassword">Password</label>
+		    		<div class="controls">
+		      			<input type="password" id="inputPass" placeholder="Password">
+		    		</div>
+		  		</div>
+		  		<div class="control-group">
+		    		<div class="controls">
+		      			<label class="checkbox">
+		        			<input type="checkbox"> Remember me
+		      			</label>
+		      			<button type="submit" id="send" class="btn">Sign in</button>
+		    		</div>
+		  		</div>
+			</form>
 
-						<form class="form-horizontal">
-		  <div class="control-group">
-		    <label class="control-label" for="inputUsername">Username</label>
-		    <div class="controls">
-		      <input type="text" id="inputUser" placeholder="Username">
-		    </div>
-		  </div>
-		  <div class="control-group">
-		    <label class="control-label" for="inputPassword">Password</label>
-		    <div class="controls">
-		      <input type="password" id="inputPass" placeholder="Password">
-		    </div>
-		  </div>
-		  <div class="control-group">
-		    <div class="controls">
-		      <label class="checkbox">
-		        <input type="checkbox"> Remember me
-		      </label>
-		      <button type="submit" id="send" class="btn">Sign in</button>
-		    </div>
-		  </div>
-	</form>
-
-				<?php require 'footer.php';?>
+			<?php require 'footer.php';?>
 		</body>
-		</html>
+	</html>
