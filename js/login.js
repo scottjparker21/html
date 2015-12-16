68167bd865b439532f349d3f71a26e754ead47d1
scottjparker21 <scottjparker21@gmail.com>
2015-12-15 18:17:27 -0600

    pushing staging to AWS through Github

--- js/login.js
+++ js/login.js
@@ -2,8 +2,8 @@
 $( document ).ready(function() {
 
 	function logmein(user,pass) {
-		
-		$.get("validate.php?username=" + user + "&password=" + pass , function (data) {
+
+		$.get("validate.php?username=" + user + "&password=" + pass , function(data) {
 			console.log(data);
 		});
 	}
--- validate.php
+++ validate.php
@@ -1,24 +1,24 @@
 <?php
 	
-			// require 'database.php';
-			// $pdo = Database::connect();
-			// $user = $_GET['username'];
-			// $pass = $_GET['password'];
-	  //       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	  //       $sql = "SELECT * FROM customer WHERE username = ? ";
-	  //       $q = $pdo->prepare($sql);
-	  //       $q->execute(array($user));
-	  //       $customer = $q->fetchAll();
-	  //       Database::disconnect(); 
+			require 'database.php';
+			$pdo = Database::connect();
+			$user = $_GET['username'];
+			$pass = $_GET['password'];
+	        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	        $sql = "SELECT * FROM customer WHERE username = ? ";
+	        $q = $pdo->prepare($sql);
+	        $q->execute(array($user));
+	        $customer = $q->fetchAll();
+	        Database::disconnect(); 
 
-	       //  $results = '';
+	        $results = '';
 
-	       // foreach ($customer as $row){ 
-			     //   	   $results .= "<p>";
-			     //   	   $results .= $row['username'];
-			     //   	   $results .= $row['password'];
-			     //   	   $results .= "</p>";
-		      //  		}
+	       foreach ($customer as $row){ 
+			       	   $results .= "<p>";
+			       	   $results .= $row['username'];
+			       	   $results .= $row['password'];
+			       	   $results .= "</p>";
+		       		}
 		       		echo "ello mate";
 		       		
 ?>
