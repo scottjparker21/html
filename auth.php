cfe106747b6874e60943f31178864111b753f039
scottjparker21 <scottjparker21@gmail.com>
2015-12-15 20:58:28 -0600

    pushing staging to AWS through Github

--- auth.php
+++ auth.php
@@ -4,22 +4,21 @@
 			$user = $_GET['user'];
 			$pass = $_GET['pass'];
 	        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	        $sql = "SELECT * FROM customer WHERE username = ? AND password = ?";
+	        $sql = "SELECT id,username,password FROM customer WHERE username = ? AND password = ?";
 	        $q = $pdo->prepare($sql);
 	        $q->execute(array($user,$pass));
-	        $customer = $q->fetchAll();
+	        $data = $q->fetch(PDO::FETCH_ASSOC);
+			$id = $data['id']	        
+	        $username = $data['username'];
+	        $password = $data["password"]
 	        Database::disconnect(); 
 
-	        $results = '';
+	     	
 
-	       foreach ($customer as $row){ 
-			//        	   $results .= "<p>";
-			       	   $results .= $row['username'];
-			       	   $results .= $row['password'];
-			//        	   $results .= "</p>";
-		       		}
 
-		       		
-		      echo $results;
+		      echo $data;
+		      
+
+		      // customer id, firstname and transaction id (set to NULL at login)
 		       		
 ?>
