58badd06ab9473ae94c8a0653d885a63b4439781
scottjparker21 <scottjparker21@gmail.com>
2015-12-15 18:14:30 -0600

    pushing staging to AWS through Github

--- validate.php
+++ validate.php
@@ -1,15 +1,15 @@
 <?php
 	
-			require 'database.php';
-			$pdo = Database::connect();
-			$user = $_GET['username'];
-			$pass = $_GET['password'];
-	        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	        $sql = "SELECT * FROM customer WHERE username = ? ";
-	        $q = $pdo->prepare($sql);
-	        $q->execute(array($user));
-	        $customer = $q->fetchAll();
-	        Database::disconnect(); 
+			// require 'database.php';
+			// $pdo = Database::connect();
+			// $user = $_GET['username'];
+			// $pass = $_GET['password'];
+	  //       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	  //       $sql = "SELECT * FROM customer WHERE username = ? ";
+	  //       $q = $pdo->prepare($sql);
+	  //       $q->execute(array($user));
+	  //       $customer = $q->fetchAll();
+	  //       Database::disconnect(); 
 
 	       //  $results = '';
 
