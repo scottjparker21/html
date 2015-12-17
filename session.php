e9182d4c28f203d1fe866874a73bdd9646f6f45f
scottjparker21 <scottjparker21@gmail.com>
2015-12-16 20:44:27 -0600

    pushing staging to AWS through Github

--- session.php
+++ session.php
@@ -2,16 +2,13 @@
 	
 	session_start();
 
-	
-
 	 if( isset( $_SESSION['userid'] ) )
 	   {
-	      print_r($_SESSION);
+	      print_r($_SESSION['userid']);
 	   }
-	   if( empty( $_SESSION['userid'] ))
+	 if( empty( $_SESSION['userid'] ))
 	   {
-	   	  print_r($_SESSION);
+	   	  print_r($_SESSION['userid']);
 	   }
-
 
 ?>
