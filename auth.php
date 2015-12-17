ef2879a5879435f44c7445b5800583d22c02a750
scottjparker21 <scottjparker21@gmail.com>
2015-12-16 21:27:00 -0600

    pushing staging to AWS through Github

--- auth.php
+++ auth.php
@@ -2,8 +2,8 @@
 
 			require 'database.php';
 			$pdo = Database::connect();
-			$user = $_GET['user'];
-			$pass = $_GET['pass'];
+			$user = ($_POST["username"]);
+			$pass = ($_POST["password"]);
 	        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	        $sql = "SELECT * FROM customer WHERE username = ? AND password = ?";
 	        $q = $pdo->prepare($sql);
--- login.php
+++ login.php
@@ -4,26 +4,28 @@
 		<?php require 'header.php'; ?>
 		<body>
 			<?php require 'navbar.php'; ?>
-		  		<div class="control-group">
-		    		<label class="control-label" for="inputUsername">Username</label>
-		    		<div class="controls">
-		      			<input type="text" id="inputUser" placeholder="Username">
-		    		</div>
-		  		</div>
-		  		<div class="control-group">
-		    		<label class="control-label" for="inputPassword">Password</label>
-		    		<div class="controls">
-		      			<input type="password" id="inputPass" placeholder="Password">
-		    		</div>
-		  		</div>
-		  		<div class="control-group">
-		    		<div class="controls">
-		      			<label class="checkbox">
-		        			<input type="checkbox"> Remember me
-		      			</label>
-		      			<button id="send" class="btn">Sign in</button>
-		    		</div>
-		  		</div>
+				<form method="post" action="auth.php" enctype="multipart/form-data">
+			  		<div class="control-group">
+			    		<label class="control-label" for="inputUsername">Username</label>
+			    		<div class="controls">
+			      			<input type="text" id="inputUser" name="username" placeholder="Username">
+			    		</div>
+			  		</div>
+			  		<div class="control-group">
+			    		<label class="control-label" for="inputPassword">Password</label>
+			    		<div class="controls">
+			      			<input type="password" name="password" id="inputPass" placeholder="Password">
+			    		</div>
+			  		</div>
+			  		<div class="control-group">
+			    		<div class="controls">
+			      			<label class="checkbox">
+			        			<input type="checkbox"> Remember me
+			      			</label>
+			      			<button id="send" type="submit" class="btn">Sign in</button>
+			    		</div>
+			  		</div>
+			  	</form>	
 
 			<?php require 'footer.php';?>
 
