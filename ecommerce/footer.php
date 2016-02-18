4398e8cefcb092828a841bf7268cfddbcc79f063
Scott Parker <scottjparker21@gmail.com>
2016-02-18 03:28:08 +0000

    beginning to fix links for crud, fixed paths for footer and header.

--- ecommerce/footer.php
+++ ecommerce/footer.php
@@ -5,7 +5,7 @@
 	     		 </div>
 	        </footer>
 
-		    <script src="js/jquery.js"></script>
-			<script src="js/bootstrap.min.js"></script>
-			<script src="js/search.js"></script>
-			<script src="js/login.js"></script>
+		    <script src="assets/js/jquery.js"></script>
+			<script src="assets/js/bootstrap.min.js"></script>
+			<script src="assets/js/search.js"></script>
+			<script src="assets/js/login.js"></script>
--- ecommerce/header.php
+++ ecommerce/header.php
@@ -3,6 +3,6 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 	</head>
